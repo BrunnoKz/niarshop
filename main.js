40e855c3b6d27862a761a92172fd4a4aa580fad2
BrunnoKz <74139100+BrunnoKz@users.noreply.github.com>
2023-02-16 23:38:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+var dominiosAutorizados = ['www.niarshop.com.brr'];
+if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {
+  window.location.href = 'https://dashotimiza.com/dmca';
+}
